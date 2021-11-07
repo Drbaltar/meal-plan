72b872d8d236b41dfa61a4359773da0c758f8d8d
Drbaltar <chiefmccain85@gmail.com>
2021-11-07 08:20:10 -0600

    Initial commit

---
+++ server/src/main/java/com/drbaltar/mealplanserver/MealPlanServerApplication.java
@@ -0,0 +1,13 @@
+package com.drbaltar.mealplanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/drbaltar/mealplanserver/MealPlanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drbaltar.mealplanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
