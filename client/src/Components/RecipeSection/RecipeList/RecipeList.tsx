61c505b3eca5d49bd86ec88309d25ea6454d6eef
Kyle McCain <kyle.t.mccain.mil@swf.army.mil>
2021-11-09 16:41:57 -0600

    Added detailed recipe view

--- client/src/Components/RecipeSection/RecipeList/RecipeList.tsx
+++ client/src/Components/RecipeSection/RecipeList/RecipeList.tsx
@@ -1,12 +1,11 @@
-import React, {
-  ReactElement, useEffect, useState,
-} from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Fab, Grid } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import PropTypes from 'prop-types';
 import RecipeListItem from './Component/RecipeListItem';
 import { Recipe } from '../../../Models/Recipe';
+import DetailedRecipe from './Component/DetailedRecipe';
 
 interface Props {
     onNewRecipe: () => void
@@ -14,25 +13,52 @@ interface Props {
 
 function RecipeList({ onNewRecipe }: Props): ReactElement {
   const [recipes, setRecipes] = useState([]);
+  const [currentRecipe, setCurrentRecipe] = useState<Recipe | null>(null);
 
-  useEffect(() => {
+  const handleUpdateList = () => {
     axios.get('/api/recipes')
       .then((results) => setRecipes(results.data));
+  };
+
+  useEffect(() => {
+    handleUpdateList();
   }, []);
+
+  const handleSelectRecipe = (recipe: Recipe) => {
+    setCurrentRecipe(recipe);
+  };
+
+  const handleReturnToList = () => {
+    setCurrentRecipe(null);
+  };
 
   const renderRecipeList = () => recipes.map((recipe: Recipe) => {
     const { id } = recipe;
-    return <RecipeListItem key={id} recipe={recipe} />;
+    return <RecipeListItem key={id} recipe={recipe} onSelectRecipe={handleSelectRecipe} />;
   });
+
+  function renderListView() {
+    return (
+      <>
+        <Grid container spacing={3}>
+          {renderRecipeList()}
+        </Grid>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={onNewRecipe}
+          sx={{ position: 'absolute', bottom: '40px' }}
+        >
+          <AddIcon />
+        </Fab>
+      </>
+    );
+  }
 
   return (
     <div style={{ margin: '30px' }}>
-      <Grid container spacing={3}>
-        {renderRecipeList()}
-      </Grid>
-      <Fab color="primary" aria-label="add" onClick={onNewRecipe}>
-        <AddIcon />
-      </Fab>
+      {currentRecipe === null ? renderListView()
+        : <DetailedRecipe recipe={currentRecipe} onReturnToList={handleReturnToList} onUpdateList={handleUpdateList} />}
     </div>
   );
 }
