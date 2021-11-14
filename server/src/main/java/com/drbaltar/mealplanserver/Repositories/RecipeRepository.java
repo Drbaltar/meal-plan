87e88f0356f9c18bd391e5a531c450a41c504344
Kyle McCain <kyle.t.mccain.mil@swf.army.mil>
2021-11-14 16:34:19 -0600

    Added custom method to find planned recipes

--- server/src/main/java/com/drbaltar/mealplanserver/Repositories/RecipeRepository.java
+++ server/src/main/java/com/drbaltar/mealplanserver/Repositories/RecipeRepository.java
@@ -4,4 +4,6 @@ import com.drbaltar.mealplanserver.Models.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+    Iterable<Recipe> findAllByIsPlannedTrue();
 }
