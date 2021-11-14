2c9a22e818e0d2816205d396fa8f616260bc27f6
Kyle McCain <kyle.t.mccain.mil@swf.army.mil>
2021-11-14 16:28:01 -0600

    Updated App to display images for home screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,76 @@
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import { ImageList, ImageListItem, Typography } from '@mui/material';
+import shoppingImage from './shopping.jpg';
+import cookingImage from './cooking.jpg';
 
 function App(): ReactElement {
   return (
-    <div>
-      <Link to="/recipes">Recipe List</Link>
-      <Link to="/groceries">Grocery List</Link>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexDirection: 'row',
+        marginTop: '20px',
+        height: '100vh',
+      }}
+    >
+      <ImageList
+        sx={{
+          width: 700, height: 'auto',
+        }}
+        cols={1}
+        gap={10}
+      >
+        <Link to="/recipes">
+          <ImageListItem>
+            <img
+              src={cookingImage}
+              alt="Recipe Section"
+              loading="lazy"
+
+            />
+            <Typography
+              variant="h1"
+              style={{
+                position: 'absolute',
+                top: '35%',
+                bottom: 'auto',
+                color: 'white',
+                right: '25%',
+                backgroundColor: 'slategray',
+              }}
+            >
+              Recipes
+            </Typography>
+          </ImageListItem>
+        </Link>
+        <Link to="/groceries">
+          <ImageListItem>
+            <img
+              src={shoppingImage}
+              alt="Grocery Section"
+              loading="lazy"
+            />
+            <Typography
+              variant="h1"
+              style={{
+                position: 'absolute',
+                top: '35%',
+                bottom: 'auto',
+                color: 'white',
+                right: '15%',
+                backgroundColor: 'slategray',
+              }}
+            >
+              Groceries
+            </Typography>
+          </ImageListItem>
+        </Link>
+      </ImageList>
     </div>
   );
 }
